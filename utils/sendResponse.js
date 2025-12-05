abaa9b75ed36a619f26088adc912b08f3f569154
Apra <apra.khanna2304@gmail.com>
2025-12-05 19:33:46 +0530

    serving mutiple assets to frontend

--- utils/sendResponse.js
+++ utils/sendResponse.js
@@ -1,4 +1,6 @@
+
 export function sendResponse(res, statsuCode, contentType, payload) {
+
   res.statusCode = statsuCode;
   res.setHeader("Content-Type", contentType);
   res.end(payload);
--- utils/serveStatic.js
+++ utils/serveStatic.js
@@ -1,11 +1,33 @@
 import path from "node:path";
 import fs from "node:fs/promises";
 import { sendResponse } from "./sendResponse.js";
+import { getContentType } from "./getContentType.js";
 export async function serveStatic(req, res, baseDir) {
-  const filePath = path.join(baseDir, "public", "index.html");
+  const filePath = path.join(
+    baseDir,
+    "public",
+    req.url === "/" ? "index.html" : req.url
+  );
 
   try {
     const content = await fs.readFile(filePath);
-    sendResponse(res, 200, "text/html", content);
-  } catch (err) {}
+    const ext = path.extname(filePath);
+    // console.log(ext);
+    const contentType = getContentType(ext);
+    sendResponse(res, 200, contentType, content);
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      const content = await fs.readFile(
+        path.join(baseDir, "public", "404.html")
+      );
+      sendResponse(res, 404, "text/html", content);
+    } else {
+      sendResponse(
+        res,
+        500,
+        "text/html",
+        "<html><h1>Server Error: ${err.code}</h1></html>"
+      );
+    }
+  }
 }
